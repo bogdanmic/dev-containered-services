1cb452568ba1f8ee575f6f6f8a086cbad14cac0c
bogdanmic <mic.bogdan@gmail.com>
2020-07-30 23:54:29 +0300

    Rename the traefik network used and simplify a bit the configs

--- install.sh
+++ install.sh
@@ -52,7 +52,7 @@ if continueYesNo "$ask"; then
     echo -e "PATH=\$PATH:$BIN_PATH" >> ~/.bashrc
 
     # Create the docker network that will be used by the services and traefik
-    docker network create dev-net-traefik
+    docker network create dev-net-trfk
 
     # Some of the services require an allready build directory path because of user rights
     # Here we create those that we know for sure that are needed. More to come ... maybe?
